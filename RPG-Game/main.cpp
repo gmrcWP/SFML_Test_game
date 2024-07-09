3bd2612ebc492f447bff6aa96d0d15ef038be65e
gmrcWP <jorgesick.roch@gmail.com>
2024-07-09 15:17:55 -0400

    Added movement

--- RPG-Game/main.cpp
+++ RPG-Game/main.cpp
@@ -1,13 +1,32 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    //---------------------------------------------INITIALIZE----------------------------------------------------
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 8;
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!", sf::Style::Default, settings);
+    
+    //---------------------------------------------INITIALIZE----------------------------------------------------
 
+    //---------------------------------------------LOAD----------------------------------------------------
+    sf::Texture playerTexture;
+    sf::Sprite playerSprite;
+
+    if (playerTexture.loadFromFile("Assets/Player/Textures/spritesheet.png")) {
+        std::cout << "Player image loaded";
+
+        int XIndex = 0;
+        int YIndex = 0;
+        playerSprite.setTexture(playerTexture);
+        playerSprite.setTextureRect(sf::IntRect(XIndex * 64, YIndex * 64, 64, 64));
+    }
+    //---------------------------------------------LOAD----------------------------------------------------
+    //main game loop
     while (window.isOpen())
     {
+        //---------------------------------------------UPDATE----------------------------------------------------
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -15,9 +34,24 @@ int main()
                 window.close();
         }
 
-        window.clear();
-        window.draw(shape);
+        sf::Vector2f position = playerSprite.getPosition();
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+            playerSprite.move(sf::Vector2f(0.1, 0));
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+            playerSprite.move(sf::Vector2f(-0.1, 0));
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+            playerSprite.move(sf::Vector2f(0, 0.1));
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+            playerSprite.move(sf::Vector2f(0, -0.1));
+        //---------------------------------------------UPDATE----------------------------------------------------
+
+
+        //---------------------------------------------DRAW----------------------------------------------------
+        window.clear(sf::Color::Black);
+        window.draw(playerSprite);
         window.display();
+        //---------------------------------------------DRAW----------------------------------------------------
     }
 
     return 0;
